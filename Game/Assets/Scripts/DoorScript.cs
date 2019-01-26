e56eb7a7d56037b255de529aaf78c1172f4acf21
Alexandr Izmaylov <zinoviy23@gmail.com>
2019-01-27 02:43:27 +0300

    Simple elevator scene

---
+++ Game/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class DoorScript : Interactive.ButtonInteractiveObject
+{
+    [SerializeField] private bool isOpen;
+    [SerializeField] private Sprite openedDoor;
+    [SerializeField] private Sprite closedDoor;
+
+    private SpriteRenderer spriteRenderer;
+    
+    protected override IEnumerator Interact()
+    {
+        yield return new WaitForSeconds(0.2f);
+        IsOpen = !IsOpen;
+    }
+
+    public bool IsOpen
+    {
+        private get { return isOpen; }
+        set
+        {
+            isOpen = value;
+
+            spriteRenderer.sprite = isOpen ? openedDoor : closedDoor;
+        }
+    }
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+}
