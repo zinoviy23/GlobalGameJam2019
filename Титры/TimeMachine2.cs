cc8ef342b175f25db8f9830d9c656c8400acdc71
TBFox <robotnxt@yandex.ru>
2019-01-27 14:50:31 +0300

    Титры

---
+++ Титры/TimeMachine2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeMachine2 : MonoBehaviour
+{
+
+    public Text text;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(Interact());
+    }
+
+    IEnumerator Interact()
+    {
+        text.text = "И после упорных трудов...";
+        yield return new WaitForSeconds(3f);
+        text.text = "Он обустроит свой дом";
+        yield return new WaitForSeconds(3f);
+        text.text = "Но станет ли это домом?";
+        yield return new WaitForSeconds(10f);
+        text.text = "Конец";
+
+        Application.Quit();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
